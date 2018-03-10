bd073d5ca952ab64a60d46006aa7602eb61d0c95
Mohammed Helmy <mohammed4bs@gmail.com>
2018-03-11 00:56:52 +0200

    First Test

---
+++ params.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'adminEmail' => 'mohammed4bs@gmail.com',
+    'apiToken'=> '9b741838e5023fe2aaa8d8f08635a8cd',
+    'apiMarker'=> '68830',
+    'apiResponseLang'=> 'en',
+    'title'=> 'Cheapest Flights',
+    'baseUrl'=> '/'
+];
